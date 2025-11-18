674d7bfe3d4c833c29bf759199203dc5bd63e933
Septian Adi Pratama <septianadip7998@gmail.com>
2025-11-18 14:28:59 +0700

    running challenge 2

--- challenge-2/main.go
+++ challenge-2/main.go
@@ -8,7 +8,6 @@ func main() {
 	C := -45
 	D := 0.67
 
-	// ubah tipe int ke float64 agar bisa dihitung bersama
 	E := (float64(A) + B) * float64(C) / D
 
 	fmt.Println("nilai dari variabel E adalah", E)
